2b55e4fab36195d6457a0153028c3b36453f6eaf
Tom Schoonjans <Tom.Schoonjans@UGent.be>
2013-04-03 16:58:53 +0200

    Small bugfix for implicit declaration warning

--- src/xmi_hdf5.c
+++ src/xmi_hdf5.c
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <string.h>
 #include <stdio.h>
 #include <glib/gprintf.h>
+#include <glib/gstdio.h>
 
 #ifdef MAC_INTEGRATION
   #import <Foundation/Foundation.h>
