1b1125a6ba169ad8dab0d8bf6881acb2acd8a6c5
Tom Schoonjans <Tom.Schoonjans@UGent.be>
2013-12-10 16:05:06 +0100

    results confirmed
    although they differ considerably from Boone, the characteristic line
    intensities seem to correspond to the reference values in Ebel 1999

--- bin/xmimsim-test-ebel.c
+++ bin/xmimsim-test-ebel.c
@@ -6,11 +6,11 @@
 int main(int argc, char *argv[]) {
 
 
-	double tube_voltage = 40.0;
+	double tube_voltage = 30.0;
 	struct xmi_layer tube_anode, tube_window, tube_filter;
 	double tube_current = 1.0;
 	double tube_angle_electron = 45.0;
-	double tube_angle_xray= 45.0;
+	double tube_angle_xray= 6.0;
 	double tube_delta_energy = 0.5;
 
 	struct xmi_excitation *excitation;
@@ -19,9 +19,9 @@ int main(int argc, char *argv[]) {
 	tube_anode.n_elements = 1;
 	tube_anode.Z = malloc(sizeof(int));
 	tube_anode.weight = malloc(sizeof(double));
-	tube_anode.Z[0] = 45;
+	tube_anode.Z[0] = 29;
 	tube_anode.weight[0] = 1.0;
-	tube_anode.density = 12.410000;
+	tube_anode.density = 8.96;
 	tube_anode.thickness= 0.000200;
 
 	tube_window.n_elements = 1;
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
 
 
 	//xmi_tube_ebel(&tube_anode, &tube_window, &tube_filter, tube_voltage, tube_current, tube_angle_electron, tube_angle_xray, 1.0,
-	xmi_tube_ebel(&tube_anode, NULL, NULL, tube_voltage, tube_current, tube_angle_electron, tube_angle_xray, tube_delta_energy, 1.0E-5, tube_transmission, &excitation);
+	xmi_tube_ebel(&tube_anode, NULL, NULL, tube_voltage, tube_current, tube_angle_electron, tube_angle_xray, tube_delta_energy, 1.0, tube_transmission, &excitation);
 
 	int i; 
 
