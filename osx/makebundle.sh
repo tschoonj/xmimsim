ad4f5f9c26dbec1e1f78e482591b93170aa6b16b
Tom Schoonjans <Tom.Schoonjans@UGent.be>
2013-12-20 16:05:06 +0000

    created 2.6 mac installer

--- osx/makebundle.sh
+++ osx/makebundle.sh
@@ -2,3 +2,5 @@ rm -rf .xmimsim-gui.app
 gtk-mac-bundler xmimsim.bundle || exit 1
 gtk-update-icon-cache --quiet XMI-MSIM.app/Contents/Resources/share/icons/hicolor
 update-mime-database XMI-MSIM.app/Contents/Resources/share/mime
+cp libintl.8.dylib libxrl.7.dylib XMI-MSIM.app/Contents/Resources/lib/
+install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Resources/lib/libiconv.2.dylib XMI-MSIM.app/Contents/Resources/lib/libintl.8.dylib
