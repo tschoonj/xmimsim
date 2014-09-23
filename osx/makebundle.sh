2e7d07c4e6ed55f4657b15bc88daf0cdd765ff54
Tom Schoonjans <Tom.Schoonjans@me.com>
2014-09-23 10:25:54 +0100

    preparing for mac os x release

--- osx/makebundle.sh
+++ osx/makebundle.sh
@@ -1,7 +1,8 @@
 rm -rf .xmimsim-gui.app 
 gtk-mac-bundler xmimsim.bundle || exit 1
-find XMI-MSIM.app -name '*.svg' | xargs rm
-gtk-update-icon-cache --quiet XMI-MSIM.app/Contents/Resources/share/icons/hicolor
+#find XMI-MSIM.app -name '*.svg' | xargs rm
+gtk-update-icon-cache --include-image-data --quiet XMI-MSIM.app/Contents/Resources/share/icons/hicolor
+gtk-update-icon-cache --include-image-data --quiet XMI-MSIM.app/Contents/Resources/share/icons/Tango
 update-mime-database XMI-MSIM.app/Contents/Resources/share/mime
 cp libintl.8.dylib libxrl.7.dylib XMI-MSIM.app/Contents/Resources/lib/
 install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Resources/lib/libiconv.2.dylib XMI-MSIM.app/Contents/Resources/lib/libintl.8.dylib
