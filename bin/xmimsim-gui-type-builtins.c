f5019bc1db5841498f7953b5bc6c88a31fde7210
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-03-27 11:28:17 +0100

    xmimsim-gui-type-builtins* now distributed in tarball
    old glib versions were not able to generate them properly...

---
+++ bin/xmimsim-gui-type-builtins.c
@@ -0,0 +1,41 @@
+
+
+
+#include "xmimsim-gui-type-builtins.h"
+#include "xmimsim-gui-compound-dialog.h"
+#include "xmimsim-gui-layer-dialog.h"
+
+/* enumerations from "xmimsim-gui-compound-dialog.h" */
+GType
+xmi_msim_gui_compound_dialog_type_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { XMI_MSIM_GUI_COMPOUND_DIALOG_ADD, "XMI_MSIM_GUI_COMPOUND_DIALOG_ADD", "add" },
+      { XMI_MSIM_GUI_COMPOUND_DIALOG_EDIT, "XMI_MSIM_GUI_COMPOUND_DIALOG_EDIT", "edit" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static(g_intern_static_string("XmiMsimGuiCompoundDialogType"), values);
+  }
+  return etype;
+}
+
+/* enumerations from "xmimsim-gui-layer-dialog.h" */
+GType
+xmi_msim_gui_layer_dialog_type_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { XMI_MSIM_GUI_LAYER_DIALOG_ADD, "XMI_MSIM_GUI_LAYER_DIALOG_ADD", "add" },
+      { XMI_MSIM_GUI_LAYER_DIALOG_EDIT, "XMI_MSIM_GUI_LAYER_DIALOG_EDIT", "edit" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static(g_intern_static_string("XmiMsimGuiLayerDialogType"), values);
+  }
+  return etype;
+}
+
+
+
---
+++ bin/xmimsim-gui-type-builtins.h
@@ -0,0 +1,19 @@
+
+
+
+#ifndef __XMIMSIM_GUI_TYPE_BUILTINS_H__
+#define __XMIMSIM_GUI_TYPE_BUILTINS_H__
+
+#include <glib-object.h>
+
+/* enumerations from "xmimsim-gui-compound-dialog.h" */
+GType xmi_msim_gui_compound_dialog_type_get_type (void);
+#define XMI_MSIM_GUI_TYPE_COMPOUND_DIALOG_TYPE (xmi_msim_gui_compound_dialog_type_get_type())
+/* enumerations from "xmimsim-gui-layer-dialog.h" */
+GType xmi_msim_gui_layer_dialog_type_get_type (void);
+#define XMI_MSIM_GUI_TYPE_LAYER_DIALOG_TYPE (xmi_msim_gui_layer_dialog_type_get_type())
+
+#endif /* __XMIMSIM_GUI_TYPE_BUILTINS_H__ */
+
+
+
