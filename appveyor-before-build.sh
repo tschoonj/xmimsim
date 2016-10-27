9d2871cfcf92d2ccbe3c76f404f7c2f88fcc55a7
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-27 11:52:58 +0100

    Appveyor: fixes part8

--- appveyor-before-build.sh
+++ appveyor-before-build.sh
@@ -39,7 +39,9 @@ if test $PLOT = "gtkextra" ; then
 	wget -T 10 -q https://downloads.sourceforge.net/project/gtkextra/3.3/gtkextra-3.3.2.tar.gz
 	tar xfz gtkextra-3.3.2.tar.gz 
 	cd gtkextra-3.3.2
-	./configure
+	# old libtool was used to generate this tarball :-(
+	autoreconf -i
+	./configure --disable-tests
 	make
 	make install
 	cd ..
