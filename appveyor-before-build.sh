de7fa46931bee61f4140231dac37c1c205654787
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-11-14 20:20:17 +0000

    AppVeyor: PLplot fixes

--- appveyor-before-build.sh
+++ appveyor-before-build.sh
@@ -81,8 +81,9 @@ elif test $PLOT = "gtkmm-plplot" ; then
 	pacman --noconfirm -Su mingw-w64-$MSYS2_ARCH-boost
 	pacman --noconfirm -Su mingw-w64-$MSYS2_ARCH-cmake
 	# install plplot from master
-   	git clone --depth 1 git://git.code.sf.net/p/plplot/plplot
+	git clone -q git://git.code.sf.net/p/plplot/plplot
 	cd plplot
+	git checkout 3ab6396f0da8e1b7109ea3323eeb0ef81d215a8a
 	cmake -G "MSYS Makefiles" -DENABLE_DYNDRIVERS=OFF -DPLD_wingcc=OFF -DCMAKE_INSTALL_PREFIX=/usr/local .
 	make
 	make install
