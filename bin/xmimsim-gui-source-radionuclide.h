761ff1888ed86e09a30f6e3f1c3d9ce4d7572039
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-11-29 17:57:00 +0000

    XmiMsimGuiSourceRadionuclide: new source

---
+++ bin/xmimsim-gui-source-radionuclide.h
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2016 Tom Schoonjans and Laszlo Vincze
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "xmimsim-gui-source-abstract.h"
+#include <gmodule.h>
+
+#ifndef XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_H
+#define XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_H
+
+G_BEGIN_DECLS
+
+#define XMI_MSIM_GUI_TYPE_SOURCE_RADIONUCLIDE (xmi_msim_gui_source_radionuclide_get_type ())
+#define XMI_MSIM_GUI_SOURCE_RADIONUCLIDE(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), XMI_MSIM_GUI_TYPE_SOURCE_RADIONUCLIDE, XmiMsimGuiSourceRadionuclide))
+#define XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), XMI_MSIM_GUI_TYPE_SOURCE_RADIONUCLIDE, XmiMsimGuiSourceRadionuclideClass))
+#define XMI_MSIM_GUI_IS_SOURCE_RADIONUCLIDE(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), XMI_MSIM_GUI_TYPE_SOURCE_RADIONUCLIDE))
+#define XMI_MSIM_GUI_IS_SOURCE_RADIONUCLIDE_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), XMI_MSIM_GUI_TYPE_SOURCE_RADIONUCLIDE))
+#define XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), XMI_MSIM_GUI_TYPE_SOURCE_RADIONUCLIDE, XmiMsimGuiSourceRadionuclideClass))
+
+typedef struct _XmiMsimGuiSourceRadionuclide XmiMsimGuiSourceRadionuclide;
+typedef struct _XmiMsimGuiSourceRadionuclideClass   XmiMsimGuiSourceRadionuclideClass;
+
+struct _XmiMsimGuiSourceRadionuclide
+{
+  	XmiMsimGuiSourceAbstract parent_instance;
+  	// all our widgets
+	GtkWidget *radioNuclideW;
+	GtkWidget *activityW;
+	GtkWidget *activityUnitW;
+	GtkWidget *nuclideSolidAngleW;
+};
+
+struct _XmiMsimGuiSourceRadionuclideClass
+{
+  XmiMsimGuiSourceAbstractClass parent_class;
+
+};
+
+GType xmi_msim_gui_source_radionuclide_get_type(void) G_GNUC_CONST;
+
+// this method needs to be exported explicitly, or it would fail on Windows
+G_MODULE_EXPORT XmiMsimGuiSourceRadionuclide* xmi_msim_gui_source_radionuclide_new(struct xmi_input *current);
+
+typedef enum {
+	XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_ERROR_INVALID_DATA,
+	XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_ERROR_GENERATE,
+	XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_ERROR_MAXIMUM,
+} XmiMsimGuiSourceRadionuclideError;
+
+#define XMI_MSIM_GUI_SOURCE_RADIONUCLIDE_ERROR (xmi_msim_gui_source_radionuclide_error_quark())
+
+GQuark xmi_msim_gui_source_radionuclide_error_quark(void);
+
+G_END_DECLS
+
+#endif
