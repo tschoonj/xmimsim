a2824889f61ca1593af9ba7d257a9d81ee5912b9
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-18 11:06:20 +0000

    XmiMsimGuiContinuousEnergyDialog: new class

--- bin/xmimsim-gui-type-builtins.c
+++ bin/xmimsim-gui-type-builtins.c
@@ -5,6 +5,7 @@
 #include "xmimsim-gui-compound-dialog.h"
 #include "xmimsim-gui-layer-dialog.h"
 #include "xmimsim-gui-discrete-energy-dialog.h"
+#include "xmimsim-gui-continuous-energy-dialog.h"
 
 /* enumerations from "xmimsim-gui-compound-dialog.h" */
 GType
@@ -50,6 +51,22 @@ xmi_msim_gui_discrete_energy_dialog_type_get_type (void)
       { 0, NULL, NULL }
     };
     etype = g_enum_register_static(g_intern_static_string("XmiMsimGuiDiscreteEnergyDialogType"), values);
+  }
+  return etype;
+}
+
+/* enumerations from "xmimsim-gui-continuous-energy-dialog.h" */
+GType
+xmi_msim_gui_continuous_energy_dialog_type_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { XMI_MSIM_GUI_CONTINUOUS_ENERGY_DIALOG_ADD, "XMI_MSIM_GUI_CONTINUOUS_ENERGY_DIALOG_ADD", "add" },
+      { XMI_MSIM_GUI_CONTINUOUS_ENERGY_DIALOG_EDIT, "XMI_MSIM_GUI_CONTINUOUS_ENERGY_DIALOG_EDIT", "edit" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static(g_intern_static_string("XmiMsimGuiContinuousEnergyDialogType"), values);
   }
   return etype;
 }
