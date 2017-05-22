a5c575583b41cb2ee4b67f86c0d013f49da4feba
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-05-22 11:17:16 +0200

    test-gui-utils-get-solid-angle-from-slits.cpp: fixes

--- tests/test-gui-utils-get-solid-angle-from-slits.cpp
+++ tests/test-gui-utils-get-solid-angle-from-slits.cpp
@@ -9,7 +9,10 @@ int main(int argc, char *argv[]) {
   //init test
   g_assert(test_init() == 1);
 
-  struct xmi_geometry geom = {.slit_size_x = 0.1, .slit_size_y = 100.0, .d_source_slit = 5.0};
+  struct xmi_geometry geom;
+  geom.slit_size_x = 0.1;
+  geom.slit_size_y = 100.0;
+  geom.d_source_slit = 5.0;
 
   double solid_angle = xmi_msim_gui_utils_get_solid_angle_from_slits(&geom);
   fprintf(stdout, "first solid angle %f\n", solid_angle);
@@ -23,7 +26,7 @@ int main(int argc, char *argv[]) {
   solid_angle = xmi_msim_gui_utils_get_solid_angle_from_slits(&geom);
   fprintf(stdout, "second solid angle %f\n", solid_angle);
 
-  g_assert(fabs(solid_angle - 0.1997936) < 1E-6);
+  g_assert(fabs(solid_angle - 0.03980015440423797) < 1E-6);
 
   return 0;
 }
