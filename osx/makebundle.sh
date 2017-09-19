77083e27e31022f8dc7271b0dfcf4abb10e71895
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-09-19 13:01:01 +0100

    Google Analytics: update macOS scripts

--- osx/makebundle.sh
+++ osx/makebundle.sh
@@ -6,6 +6,8 @@ gtk-update-icon-cache --include-image-data --quiet XMI-MSIM.app/Contents/Resourc
 update-mime-database XMI-MSIM.app/Contents/Resources/share/mime
 mkdir -p XMI-MSIM.app/Contents/Resources/etc/gtk-3.0
 cp settings.ini XMI-MSIM.app/Contents/Resources/etc/gtk-3.0/
+mkdir -p XMI-MSIM.app/Contents/Library/QuickLook
+cp -a ../build/osx/quicklook/xmi-msim.qlgenerator XMI-MSIM.app/Contents/Library/QuickLook/
 #cp libintl.8.dylib libxrl.7.dylib XMI-MSIM.app/Contents/Resources/lib/
 #install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Resources/lib/libiconv.2.dylib XMI-MSIM.app/Contents/Resources/lib/libintl.8.dylib
 #cp /usr/local/share/xmimsim/xmimsimdata.h5 XMI-MSIM.app/Contents/Resources/
