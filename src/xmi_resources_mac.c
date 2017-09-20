6f19ecf8c00f0027bf3d62f64778175629f9049d
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-09-20 13:16:24 +0100

    Fix macOS resources retrieval

--- src/xmi_resources_mac.c
+++ src/xmi_resources_mac.c
@@ -25,19 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <CoreFoundation/CoreFoundation.h>
 #include <glib.h>
 
-char* xmi_application_get_resource_path() {
-	char *str = NULL;
-	CFBundleRef bundle = CFBundleGetMainBundle();
-	if (!bundle)
-		return NULL;
-	CFURLRef url = CFBundleCopyResourcesDirectoryURL(bundle);
-	CFStringRef path = CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
-	CFRelease(url);
-	str = g_strdup(CFStringGetCStringPtr(path, kCFStringEncodingUTF8));
-	CFRelease(path);
-	return str;
-}
-
 char* xmi_application_get_bundle_path() {
 	char *str = NULL;
 	CFBundleRef bundle = CFBundleGetMainBundle();
@@ -51,53 +38,52 @@ char* xmi_application_get_bundle_path() {
 	return str;
 }
 
-
 int xmi_resources_mac_query(int kind, char **resource_file) {
-	gchar *resource_path;
+	gchar *bundle_path;
 	gchar *temp;
 
-	resource_path = xmi_application_get_resource_path();
-	if (resource_path == NULL) {
-		fprintf(stderr, "Could not get resource path!\n");
+	bundle_path = xmi_application_get_bundle_path();
+	if (bundle_path == NULL) {
+		fprintf(stderr, "Could not get bundle path!\n");
 		return 0;
 	}
 
 	switch (kind) {
 		case XMI_RESOURCES_MAC_DATA:
-			temp = g_strdup_printf("%s/xmimsimdata.h5",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmimsimdata.h5",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMSO2XMSI:
-			temp = g_strdup_printf("%s/xmso2xmsi.xml",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmso2xmsi.xml",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMSO2SVG:
-			temp = g_strdup_printf("%s/xmso2svg.xml",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmso2svg.xml",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMSO2SPE:
-			temp = g_strdup_printf("%s/xmso2spe.xml",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmso2spe.xml",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMSO2CSV:
-			temp = g_strdup_printf("%s/xmso2csv.xml",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmso2csv.xml",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMSO2HTM:
-			temp = g_strdup_printf("%s/xmso2htm.xml",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmso2htm.xml",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMIMSIM_EXEC:
-			temp = g_strdup_printf("%s/xmimsim",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmimsim",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_OPENCL_CODE:
-			temp = g_strdup_printf("%s",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_OPENCL_LIB:
-			temp = g_strdup_printf("%s",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_XMSA2XMSO:
-			temp = g_strdup_printf("%s/xmsa2xmso.xml",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/xmsa2xmso.xml",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_SOURCES:
-			temp = g_strdup_printf("%s/sources",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/sources",bundle_path);
 			break;
 		case XMI_RESOURCES_MAC_COORDINATE_SYSTEM:
-			temp = g_strdup_printf("%s/coordinate_system.png",resource_path);
+			temp = g_strdup_printf("%s/Contents/Resources/coordinate_system.png",bundle_path);
 			break;
 		default:
 			fprintf(stderr,"Invalid kind in xmi_resources_mac_query\n");
@@ -105,7 +91,7 @@ int xmi_resources_mac_query(int kind, char **resource_file) {
 
 	}
 	
-	g_free(resource_path);
+	g_free(bundle_path);
 	*resource_file = temp;
 	return 1;
 }
