aacceb841ca07d40c3315f51dfc40639a58e520a
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-10-11 15:38:33 +0200

    XmiMsimGuiXmsiScrolledWindow: new class
    Refactor of xmimsim-gui-batch.cpp

--- bin/xmimsim-gui-batch.h
+++ bin/xmimsim-gui-batch.h
@@ -24,26 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void batchmode_button_clicked_cb(GtkWidget *button, GtkWidget *window);
 
-enum {
-	INPUT_PARAMETER_COLUMN,
-	INPUT_VALUE_COLUMN,
-	INPUT_SELECTABLE_COLUMN,
-	INPUT_XPATH_COLUMN,
-	INPUT_ALLOWED_COLUMN,
-	INPUT_N_COLUMNS
-};
-
-GtkWidget *get_inputfile_treeview(struct xmi_input *input, int with_colors);
-
-enum {
-	PARAMETER_DOUBLE = 1,
-	PARAMETER_INT = 2,
-	PARAMETER_LONG = 4,
-	PARAMETER_STRICT_POSITIVE = 8,
-	PARAMETER_POSITIVE = 16,
-	PARAMETER_WEIGHT_FRACTION = 32,
-};
-
 void launch_archive_plot(struct xmi_archive *archive, GtkWidget *window);
 
 
