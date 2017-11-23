0f684f8799569413a29099b42f1619edf06a838c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-11-23 14:46:24 +0000

    AppVeyor: make nightly builds

--- appveyor-before-build.sh
+++ appveyor-before-build.sh
@@ -3,15 +3,16 @@
 set -e
 set -x
 
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export PKG_CONFIG_PATH=$HOME/install/lib/pkgconfig
+export PATH=$HOME/install/bin:$PATH
 export GTKMM_PLPLOT_BRANCH=plot-objects
 
 # install xraylib
 wget -q https://xraylib.tomschoonjans.eu/xraylib-3.3.0.tar.gz
 tar xfz xraylib-3.3.0.tar.gz
 cd xraylib-3.3.0
-./configure
-make
+./configure --prefix=$HOME/install
+make -j2
 make install
 cd ..
 
@@ -19,7 +20,11 @@ cd ..
 wget -q https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.12/src/hdf5-1.8.12.tar.gz
 tar xfz hdf5-1.8.12.tar.gz 
 cd hdf5-1.8.12
-./configure --disable-hl --prefix=/usr/local CPPFLAGS=-D_GNU_SOURCE=1
+# add support for UTF-8 filenames
+wget -q https://www.dropbox.com/s/gowzeo6vdhjpxnw/hdf5-1.8.12.diff
+patch -p1 < hdf5-1.8.12.diff
+autoreconf -i
+./configure --disable-fortran --disable-cxx --disable-hl --prefix=$HOME/install CPPFLAGS=-D_GNU_SOURCE=1
 # patch hdf5 -> https://tschoonj.github.io/blog/2014/01/29/building-a-64-bit-version-of-hdf5-with-mingw-w64/
 echo "#ifndef H5_HAVE_WIN32_API" >> src/H5pubconf.h
 echo "#ifdef WIN32 /* defined for all windows systems */" >> src/H5pubconf.h
@@ -29,10 +34,11 @@ echo "#endif" >> src/H5pubconf.h
 echo "#ifndef H5_HAVE_MINGW" >> src/H5pubconf.h
 echo "#ifdef __MINGW32__ /*defined for all MinGW compilers */" >> src/H5pubconf.h
 echo "#define H5_HAVE_MINGW 1" >> src/H5pubconf.h
+echo "#define H5_HAVE_WINDOWS 1" >> src/H5pubconf.h
 echo "#endif" >> src/H5pubconf.h
 echo "#endif" >> src/H5pubconf.h
 echo "#define H5_BUILT_AS_DYNAMIC_LIB 1" >> src/H5pubconf.h
-make
+make -j2
 make install
 cd ..
 
@@ -44,7 +50,7 @@ if test $RNG = "fgsl" ; then
 	wget -q http://www.lrz.de/services/software/mathematik/gsl/fortran/download/fgsl-1.1.0.tar.gz
 	tar xfz fgsl-1.1.0.tar.gz
 	cd fgsl-1.1.0
-	./configure
+	./configure --prefix=$HOME/install 
 	make
 	make install
 	cd ..
@@ -52,8 +58,8 @@ elif test $RNG = "easyRNG" ; then
 	wget -q https://github.com/tschoonj/easyRNG/releases/download/easyRNG-1.1/easyRNG-1.1.tar.gz
 	tar xfz easyRNG-1.1.tar.gz
 	cd easyRNG-1.1
-	./configure
-	make
+	./configure --prefix=$HOME/install 
+	make -j2
 	make install
 	cd ..
 else
@@ -70,11 +76,11 @@ elif test $PLOT = "gtkmm-plplot" ; then
 	# install plplot from master
 	#git clone --depth 1 -q git@github.com:PLplot/PLplot.git
 	#cd plplot
-	wget -q http://lvserver.ugent.be/~schoon/plplot-5.12.0.tar.gz
-	tar xvfz plplot-5.12.0.tar.gz
-	cd plplot-5.12.0
-	cmake -G "MSYS Makefiles" -DENABLE_DYNDRIVERS=OFF -DPLD_wingcc=OFF -DCMAKE_INSTALL_PREFIX=/usr/local .
-	make
+	wget -q http://lvserver.ugent.be/~schoon/plplot-5.13.0.tar.gz
+	tar xfz plplot-5.13.0.tar.gz
+	cd plplot-5.13.0
+	cmake -G "MSYS Makefiles" -DENABLE_fortran=OFF -DENABLE_tcl=OFF -DENABLE_tk=OFF -DENABLE_DYNDRIVERS=OFF -DPLD_wingcc=OFF -DCMAKE_INSTALL_PREFIX=$HOME/install .
+	make -j2
 	make install
 	cd ..
 	# install gtkmm-plplot
@@ -87,8 +93,8 @@ elif test $PLOT = "gtkmm-plplot" ; then
 	  tar xfz gtkmm-plplot-2.2.tar.gz
 	  cd gtkmm-plplot-2.2
         fi
-	./configure --prefix=/usr/local
-	make
+	./configure --prefix=$HOME/install
+	make -j2
 	make install
 	cd ..
 	if test $UPDATER = "true" ; then
@@ -101,4 +107,8 @@ if test -z ${GOOGLE_ANALYTICS+x} ; then
 	:
 else
 	pacman --noconfirm -Su mingw-w64-$MSYS2_ARCH-libsoup
+	pushd /usr/local
+	wget -q https://www.dropbox.com/s/l6pw1dupx81ulzv/opencl-win64-devel.tar.gz
+	tar xfz opencl-win64-devel.tar.gz
+	popd
 fi
