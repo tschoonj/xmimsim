2d3285e0d7d354bfb634117f415dbe1694240ab2
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-12-20 20:51:30 +0100

    CI: disable deps static libraries

--- appveyor-before-build.sh
+++ appveyor-before-build.sh
@@ -11,7 +11,7 @@ export GTKMM_PLPLOT_BRANCH=plot-objects
 wget -q https://xraylib.tomschoonjans.eu/xraylib-3.3.0.tar.gz
 tar xfz xraylib-3.3.0.tar.gz
 cd xraylib-3.3.0
-./configure --prefix=$HOME/install
+./configure --prefix=$HOME/install --disable-static
 make -j2
 make install
 cd ..
@@ -24,7 +24,7 @@ cd hdf5-1.8.12
 wget -q https://www.dropbox.com/s/gowzeo6vdhjpxnw/hdf5-1.8.12.diff
 patch -p1 < hdf5-1.8.12.diff
 autoreconf -i
-./configure --disable-fortran --disable-cxx --disable-hl --prefix=$HOME/install CPPFLAGS=-D_GNU_SOURCE=1
+./configure --disable-fortran --disable-cxx --disable-hl --prefix=$HOME/install --disable-static CPPFLAGS=-D_GNU_SOURCE=1
 # patch hdf5 -> https://tschoonj.github.io/blog/2014/01/29/building-a-64-bit-version-of-hdf5-with-mingw-w64/
 echo "#ifndef H5_HAVE_WIN32_API" >> src/H5pubconf.h
 echo "#ifdef WIN32 /* defined for all windows systems */" >> src/H5pubconf.h
@@ -50,7 +50,7 @@ if test $RNG = "fgsl" ; then
 	wget -q http://www.lrz.de/services/software/mathematik/gsl/fortran/download/fgsl-1.1.0.tar.gz
 	tar xfz fgsl-1.1.0.tar.gz
 	cd fgsl-1.1.0
-	./configure --prefix=$HOME/install 
+	./configure --prefix=$HOME/install --disable-static
 	make
 	make install
 	cd ..
@@ -58,7 +58,7 @@ elif test $RNG = "easyRNG" ; then
 	wget -q https://github.com/tschoonj/easyRNG/releases/download/easyRNG-1.1/easyRNG-1.1.tar.gz
 	tar xfz easyRNG-1.1.tar.gz
 	cd easyRNG-1.1
-	./configure --prefix=$HOME/install 
+	./configure --prefix=$HOME/install --disable-static
 	make -j2
 	make install
 	cd ..
@@ -93,7 +93,7 @@ elif test $PLOT = "gtkmm-plplot" ; then
 	  tar xfz gtkmm-plplot-2.2.tar.gz
 	  cd gtkmm-plplot-2.2
         fi
-	./configure --prefix=$HOME/install
+	./configure --prefix=$HOME/install --disable-static
 	make -j2
 	make install
 	cd ..
