725ce88ca2b80b41721a2d8c76126cc32a09962d
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-01 16:42:13 +0000

    xmi_transmission_efficiency: use GFileInputStream instead of fopen

--- src/xmi_transmission_efficiency.c
+++ src/xmi_transmission_efficiency.c
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "config.h"
-#include <glib/gstdio.h>
+#include <gio/gio.h>
 #include <string.h>
 #include "xmi_aux.h"
 #include "xmi_private.h"
@@ -27,26 +27,43 @@ gboolean xmi_transmission_efficiency_read(const char *filename, size_t *nefficie
 	GArray *eff_x = g_array_new(FALSE, FALSE, sizeof(double));
 	GArray *eff_y = g_array_new(FALSE, FALSE, sizeof(double));
 
-	FILE *fp;
-	if ((fp = g_fopen(filename, "r")) == NULL) {
-		g_set_error(error, XMI_MSIM_ERROR, XMI_MSIM_ERROR_TRANSMISSION_EFFICIENCY, "Could not open %s for reading", filename);
+	GFile *file = g_file_new_for_path(filename);
+	GFileInputStream *file_stream = g_file_read(file, NULL, error);
+	g_object_unref(file);
+	if (!file_stream) {
 		g_array_free(eff_x, TRUE);
 		g_array_free(eff_y, TRUE);
 		return FALSE;
 	}
 
-	char *line = NULL;
+	GDataInputStream *data_stream = g_data_input_stream_new(G_INPUT_STREAM(file_stream));
+	g_data_input_stream_set_newline_type(data_stream, G_DATA_STREAM_NEWLINE_TYPE_ANY);
+
+	char *line = (char *) 1;
 	double energy, efficiency;
-	ssize_t linelen;
-	size_t linecap = 0;
 	int values;
-	while ((linelen = getline(&line, &linecap, fp)) > -1) {
+	while (line) {
+		gsize linelen;
+		GError *tmp_error = NULL;
+		line = g_data_input_stream_read_line(data_stream, &linelen, NULL, &tmp_error);
+		if (tmp_error != NULL) {
+			g_propagate_error(error, tmp_error);
+			g_object_unref(file_stream);
+			g_object_unref(data_stream);
+			g_array_free(eff_x, TRUE);
+			g_array_free(eff_y, TRUE);
+			return FALSE;
+		}
+		else if (line == NULL)
+			break;
 		if (linelen == 0 || strlen(g_strstrip(line)) == 0) {
 			continue;
 		}
 		values = sscanf(line,"%lg %lg", &energy, &efficiency);
 		if (values != 2 || energy < 0.0 || efficiency < 0.0 || efficiency > 1.0 || (eff_x->len > 0 && energy <= g_array_index(eff_x, double, eff_x->len - 1)) || (eff_x->len == 0 && energy >= 1.0)) {
 			g_set_error(error, XMI_MSIM_ERROR, XMI_MSIM_ERROR_TRANSMISSION_EFFICIENCY, "Error reading %s. The transmission efficiency file should contain two columns with energies (keV) in the left column and the transmission efficiency (value between 0 and 1) in the second column. Empty lines are ignored. First energy must be between 0 and 1 keV. The last value must be greater or equal to the tube voltage. At least 10 values are required.", filename);
+			g_object_unref(file_stream);
+			g_object_unref(data_stream);
 			g_array_free(eff_x, TRUE);
 			g_array_free(eff_y, TRUE);
 			return FALSE;
@@ -54,12 +71,13 @@ gboolean xmi_transmission_efficiency_read(const char *filename, size_t *nefficie
 		g_array_append_val(eff_x, energy);
 		g_array_append_val(eff_y, efficiency);
 		g_free(line);
-		line = NULL;
 	}
-	fclose(fp);
 
-	if (eff_x->len < 10/* || xep->tube_voltage > g_array_index(eff_x, double, eff_x->len - 1)*/) {
-		g_set_error(error, XMI_MSIM_ERROR, XMI_MSIM_ERROR_TRANSMISSION_EFFICIENCY, "Error reading %s. The transmission efficiency file should contain two columns with energies (keV) in the left column and the transmission efficiency (value between 0 and 1) in the second column. Empty lines are ignored. First energy must be between 0 and 1 keV. The last value must be greater or equal to the tube voltage. At least 10 values are required.", filename);
+	g_object_unref(file_stream);
+	g_object_unref(data_stream);
+
+	if (eff_x->len < 10) {
+		g_set_error(error, XMI_MSIM_ERROR, XMI_MSIM_ERROR_TRANSMISSION_EFFICIENCY, "Error reading %s. The transmission efficiency file should contain two columns with energies (keV) in the left column and the transmission efficiency (value between 0 and 1) in the second column. Empty lines are ignored. First energy must be between 0 and 1 keV. At least 10 values are required.", filename);
 		g_array_free(eff_x, TRUE);
 		g_array_free(eff_y, TRUE);
 		return FALSE;
