07355bd61d95b7386ee9f1f722569b7aa6dbf732
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-16 12:40:17 +0000

    XmiMsimGuiClipboardManager: new class

---
+++ bin/xmimsim-gui-clipboard-manager.c
@@ -0,0 +1,247 @@
+/*
+Copyright (C) 2018 Tom Schoonjans and Laszlo Vincze
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config.h>
+#include "xmimsim-gui-clipboard-manager.h"
+#include "xmimsim-gui-marshal.h"
+#include "xmimsim-gui-layer-box.h"
+
+#define LayerAtom gdk_atom_intern_static_string("xmi-msim-layer")
+
+typedef struct {
+	GArray *signal_ids; // gulongs!
+} XmiMsimGuiClipboardManagerHashValue;
+
+static void clipboard_manager_hash_value_free(XmiMsimGuiClipboardManagerHashValue *hash_value) {
+	g_array_free(hash_value->signal_ids, TRUE);
+	g_free(hash_value);
+}
+
+struct _XmiMsimGuiClipboardManager {
+	GObject parent_instance;
+	GHashTable *hash_table;
+	GtkWidget *focus_widget;
+};
+
+struct _XmiMsimGuiClipboardManagerClass {
+	GObjectClass parent_class;
+};
+
+G_DEFINE_TYPE(XmiMsimGuiClipboardManager, xmi_msim_gui_clipboard_manager, G_TYPE_OBJECT)
+
+enum {
+	UPDATE_CLIPBOARD_BUTTONS,
+	LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL];
+
+static void xmi_msim_gui_clipboard_manager_dispose(GObject *gobject) {
+	G_OBJECT_CLASS(xmi_msim_gui_clipboard_manager_parent_class)->dispose(gobject);
+}
+
+static void xmi_msim_gui_clipboard_manager_finalize(GObject *gobject) {
+	XmiMsimGuiClipboardManager *manager = XMI_MSIM_GUI_CLIPBOARD_MANAGER(gobject);
+
+	g_hash_table_destroy(manager->hash_table);
+
+	G_OBJECT_CLASS(xmi_msim_gui_clipboard_manager_parent_class)->finalize(gobject);
+}
+
+static void xmi_msim_gui_clipboard_manager_class_init(XmiMsimGuiClipboardManagerClass *klass) {
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+	object_class->dispose = xmi_msim_gui_clipboard_manager_dispose;
+	object_class->finalize = xmi_msim_gui_clipboard_manager_finalize;
+	
+	signals[UPDATE_CLIPBOARD_BUTTONS] = g_signal_new(
+		"update-clipboard-buttons",
+		G_TYPE_FROM_CLASS(klass),
+		G_SIGNAL_RUN_LAST,
+		0, // no default handler
+		NULL,
+		NULL,
+		xmi_msim_gui_VOID__BOOLEAN_BOOLEAN,
+		G_TYPE_NONE,
+		2,
+		G_TYPE_BOOLEAN,// GBOOLEAN -> CUT/COPY
+		G_TYPE_BOOLEAN // GBOOLEAN -> PASTE
+	);
+}
+
+static void xmi_msim_gui_clipboard_manager_init(XmiMsimGuiClipboardManager *manager) {
+	manager->hash_table = g_hash_table_new_full(g_direct_hash, g_direct_equal, NULL, (GDestroyNotify) clipboard_manager_hash_value_free);
+	manager->focus_widget = NULL;
+}
+
+XmiMsimGuiClipboardManager* xmi_msim_gui_clipboard_manager_new() {
+	return XMI_MSIM_GUI_CLIPBOARD_MANAGER(g_object_new(XMI_MSIM_GUI_TYPE_CLIPBOARD_MANAGER, NULL));
+}
+
+static void layer_box_update_clipboard_buttons(XmiMsimGuiLayerBox *box, gboolean cut_copy_status, gboolean paste_status, XmiMsimGuiClipboardManager *manager) {
+	if (paste_status)
+		manager->focus_widget = GTK_WIDGET(box);
+	else
+		manager->focus_widget = NULL;
+
+	g_signal_emit(manager, signals[UPDATE_CLIPBOARD_BUTTONS], 0, cut_copy_status, paste_status && gtk_clipboard_wait_is_target_available(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), LayerAtom));
+}
+
+static gboolean entry_focus_out_cb(GtkEntry *entry, GdkEvent *event, XmiMsimGuiClipboardManager *manager) {
+	//make sure possible selections are removed
+	manager->focus_widget = NULL;
+	gtk_editable_select_region(GTK_EDITABLE(entry), 0, 0);
+
+	g_signal_emit(manager, signals[UPDATE_CLIPBOARD_BUTTONS], 0, FALSE, FALSE);
+
+	return FALSE;
+}
+
+static gboolean entry_focus_in_cb(GtkEntry *entry, GdkEvent *event, XmiMsimGuiClipboardManager *manager) {
+	manager->focus_widget = GTK_WIDGET(entry);
+	gboolean paste_status = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD) && gtk_clipboard_wait_is_text_available(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD));
+
+	g_signal_emit(manager, signals[UPDATE_CLIPBOARD_BUTTONS], 0, FALSE, paste_status);
+
+	return FALSE;
+}
+
+static void entry_notify_cursor_position_cb(GObject *entry, GParamSpec *pspec, XmiMsimGuiClipboardManager *manager) {
+	//ignore if not in focus
+	if (!gtk_widget_has_focus(GTK_WIDGET(entry))) {
+		return;
+	}
+
+	gint selection_bound;
+	gint current_pos;
+
+	g_object_get(entry, "selection-bound", &selection_bound, "cursor-position", &current_pos, NULL);
+
+	gboolean cut_copy_status = selection_bound != current_pos;
+	gboolean paste_status = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD) && gtk_clipboard_wait_is_text_available(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD));
+
+	g_signal_emit(manager, signals[UPDATE_CLIPBOARD_BUTTONS], 0, cut_copy_status, paste_status);
+}
+
+void xmi_msim_gui_clipboard_manager_register_widget(XmiMsimGuiClipboardManager *manager, GtkWidget *widget) {
+	g_return_if_fail(XMI_MSIM_GUI_IS_CLIPBOARD_MANAGER(manager));
+	g_return_if_fail(GTK_IS_WIDGET(widget));
+
+	XmiMsimGuiClipboardManagerHashValue *hv = g_malloc(sizeof(XmiMsimGuiClipboardManagerHashValue));
+	hv->signal_ids = g_array_new(FALSE, FALSE, sizeof(gulong));
+
+	if (XMI_MSIM_GUI_IS_LAYER_BOX(widget)) {
+		gulong signal_id = g_signal_connect(widget, "update-clipboard-buttons", G_CALLBACK(layer_box_update_clipboard_buttons), manager);
+		g_array_append_val(hv->signal_ids, signal_id);
+	}
+	else if (GTK_IS_ENTRY(widget)) {
+		gulong signal_ids[4];
+		signal_ids[0] = g_signal_connect(G_OBJECT(widget), "focus-out-event", G_CALLBACK(entry_focus_out_cb), manager);
+		signal_ids[1] = g_signal_connect(G_OBJECT(widget), "focus-in-event", G_CALLBACK(entry_focus_in_cb), manager);
+		signal_ids[2] = g_signal_connect(G_OBJECT(widget), "notify::cursor-position", G_CALLBACK(entry_notify_cursor_position_cb), manager);
+		signal_ids[3] = g_signal_connect(G_OBJECT(widget), "notify::selection-bound", G_CALLBACK(entry_notify_cursor_position_cb), manager);
+		g_array_append_vals(hv->signal_ids, signal_ids, 4);
+	}
+	else {
+		g_critical("xmi_msim_gui_clipboard_manager_register_widget: unsupported widget type %s detected", g_type_name(G_OBJECT_TYPE(widget)));
+		clipboard_manager_hash_value_free(hv);
+		return;
+	}
+
+	g_hash_table_insert(manager->hash_table, widget, hv);
+}
+
+GtkWidget* xmi_msim_gui_clipboard_manager_get_focus_widget(XmiMsimGuiClipboardManager *manager) {
+	g_return_val_if_fail(XMI_MSIM_GUI_IS_CLIPBOARD_MANAGER(manager), NULL);
+
+	return manager->focus_widget;
+}
+
+void xmi_msim_gui_clipboard_manager_cut(XmiMsimGuiClipboardManager *manager) {
+	GtkWidget *focus_widget = manager->focus_widget;
+
+	if (focus_widget == NULL) {
+		g_warning("xmi_msim_gui_clipboard_manager_cut: focus_widget is NULL");
+	}
+	else if (GTK_IS_TEXT_VIEW(focus_widget)) {
+		g_signal_emit_by_name(G_OBJECT(focus_widget), "cut-clipboard", NULL);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteT), TRUE);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteW), TRUE);
+	}
+	else if (GTK_IS_ENTRY(focus_widget)) {
+		g_signal_emit_by_name(G_OBJECT(focus_widget), "cut-clipboard", NULL);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteT), TRUE);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteW), TRUE);
+	}
+	else if (XMI_MSIM_GUI_IS_LAYER_BOX(focus_widget)) {
+		xmi_msim_gui_layer_box_clipboard_cut(XMI_MSIM_GUI_LAYER_BOX(focus_widget));
+	}
+	else {
+		g_warning("xmi_msim_gui_clipboard_manager_cut: unknown focus_widget type");
+	}
+}
+
+void xmi_msim_gui_clipboard_manager_copy(XmiMsimGuiClipboardManager *manager) {
+	GtkWidget *focus_widget = manager->focus_widget;
+
+	if (focus_widget == NULL) {
+		g_warning("xmi_msim_gui_clipboard_manager_copy: focus_widget is NULL");
+	}
+	else if (GTK_IS_TEXT_VIEW(focus_widget)) {
+		g_signal_emit_by_name(G_OBJECT(focus_widget), "copy-clipboard", NULL);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteT), TRUE);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteW), TRUE);
+	}
+	else if (GTK_IS_ENTRY(focus_widget)) {
+		g_signal_emit_by_name(G_OBJECT(focus_widget), "copy-clipboard", NULL);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteT), TRUE);
+		//gtk_widget_set_sensitive(GTK_WIDGET(pasteW), TRUE);
+	}
+	else if (XMI_MSIM_GUI_IS_LAYER_BOX(focus_widget)) {
+		xmi_msim_gui_layer_box_clipboard_copy(XMI_MSIM_GUI_LAYER_BOX(focus_widget));
+	}
+	else {
+		g_warning("xmi_msim_gui_clipboard_manager_copy: unknown focus_widget type");
+	}
+}
+
+void xmi_msim_gui_clipboard_manager_paste(XmiMsimGuiClipboardManager *manager) {
+	GtkWidget *focus_widget = manager->focus_widget;
+	GtkClipboard *clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
+
+	if (!clipboard) {
+		g_warning("xmi_msim_gui_clipboard_manager_paste: could not get clipboard");
+		return;
+	}
+
+	if (focus_widget == NULL) {
+		g_warning("xmi_msim_gui_clipboard_manager_paste: focus_widget is NULL");
+	}
+	else if (GTK_IS_TEXT_VIEW(focus_widget) && gtk_clipboard_wait_is_text_available(clipboard)) {
+		g_signal_emit_by_name(G_OBJECT(focus_widget), "paste-clipboard", NULL);
+	}
+	else if (GTK_IS_ENTRY(focus_widget) && gtk_clipboard_wait_is_text_available(clipboard)) {
+		g_signal_emit_by_name(G_OBJECT(focus_widget), "paste-clipboard", NULL);
+	}
+	else if (XMI_MSIM_GUI_IS_LAYER_BOX(focus_widget) && gtk_clipboard_wait_is_target_available(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), LayerAtom)) {
+		xmi_msim_gui_layer_box_clipboard_paste(XMI_MSIM_GUI_LAYER_BOX(focus_widget));
+	}
+	else {
+		g_warning("xmi_msim_gui_clipboard_manager_paste: unknown focus_widget type");
+	}
+
+}
--- bin/xmimsim-gui-layer-box-test.c
+++ bin/xmimsim-gui-layer-box-test.c
@@ -17,7 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "xmimsim-gui-layer-box.h"
 #include "xmimsim-gui-colors.h"
+#include "xmimsim-gui-clipboard-manager.h"
 #include "xmi_xml.h"
+
+GtkWidget *copy, *cut, *paste;
 
 static gboolean delete_event( GtkWidget *widget,
                               GdkEvent  *event,
@@ -49,6 +52,12 @@ static void changed(XmiMsimGuiLayerBox *lb, gchar *change, gpointer data) {
 	g_free(layers);
 }
 
+void update_clipboard_buttons(XmiMsimGuiClipboardManager *clipboard_manager, gboolean cut_copy_val, gboolean paste_val, gpointer data) {
+	gtk_widget_set_sensitive(cut, cut_copy_val);
+	gtk_widget_set_sensitive(copy, cut_copy_val);
+	gtk_widget_set_sensitive(paste, paste_val);
+}
+
 int main(int argc, char *argv[]) {
 
 
@@ -64,13 +73,62 @@ int main(int argc, char *argv[]) {
 	g_signal_connect (window, "destroy",
 		      G_CALLBACK (destroy), NULL);
 	gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+
+	GtkWidget *grid = gtk_grid_new();
+	gtk_widget_set_halign(grid, GTK_ALIGN_FILL);
+	gtk_widget_set_valign(grid, GTK_ALIGN_FILL);
+	gtk_widget_set_hexpand(grid, TRUE);
+	gtk_widget_set_vexpand(grid, TRUE);
+
+	copy = gtk_button_new_with_label("Copy");
+	cut = gtk_button_new_with_label("Cut");
+	paste = gtk_button_new_with_label("Paste");
+
+	gtk_widget_set_halign(copy, GTK_ALIGN_END);
+	gtk_widget_set_halign(cut, GTK_ALIGN_CENTER);
+	gtk_widget_set_halign(paste, GTK_ALIGN_START);
+	gtk_widget_set_valign(copy, GTK_ALIGN_START);
+	gtk_widget_set_valign(cut, GTK_ALIGN_START);
+	gtk_widget_set_valign(paste, GTK_ALIGN_START);
+	gtk_widget_set_hexpand(copy, TRUE);
+	gtk_widget_set_hexpand(paste, TRUE);
+
+	gtk_grid_attach(GTK_GRID(grid), copy, 0, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), cut, 1, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), paste, 2, 0, 1, 1);
+
+	gtk_widget_set_sensitive(copy, FALSE);
+	gtk_widget_set_sensitive(cut, FALSE);
+	gtk_widget_set_sensitive(paste, FALSE);
+	gtk_widget_set_can_focus(copy, FALSE);
+	gtk_widget_set_can_focus(cut, FALSE);
+	gtk_widget_set_can_focus(paste, FALSE);
+
 	GtkWidget *lb = xmi_msim_gui_layer_box_new(XMI_MSIM_GUI_LAYER_BOX_TYPE_COMPOSITION);
 	int Z[3] = {12, 18, 26};
 	double weight[3] = {0.3, 0.5, 0.2};
 	struct xmi_layer layer = {.n_elements = 3, .Z = Z, .weight = weight, .density = 4.5, .thickness = 1.3};
 	xmi_msim_gui_layer_box_set_layers(XMI_MSIM_GUI_LAYER_BOX(lb), 1, &layer, 0);
-	gtk_container_add(GTK_CONTAINER(window), lb);
 	g_signal_connect(G_OBJECT(lb), "changed", G_CALLBACK(changed), NULL);
+	gtk_grid_attach(GTK_GRID(grid), lb, 0, 1, 3, 1);
+	
+	GtkWidget *lb2 = xmi_msim_gui_layer_box_new(XMI_MSIM_GUI_LAYER_BOX_TYPE_EXCITATION_ABSORBERS);
+	g_signal_connect(G_OBJECT(lb2), "changed", G_CALLBACK(changed), NULL);
+	gtk_grid_attach(GTK_GRID(grid), lb2, 0, 2, 3, 1);
+
+	GtkWidget *entry = gtk_entry_new();
+	gtk_grid_attach(GTK_GRID(grid), entry, 0, 3, 3, 1);
+
+	gtk_container_add(GTK_CONTAINER(window), grid);
+
+	XmiMsimGuiClipboardManager *clipboard_manager = xmi_msim_gui_clipboard_manager_new();
+	xmi_msim_gui_clipboard_manager_register_widget(clipboard_manager, lb);
+	xmi_msim_gui_clipboard_manager_register_widget(clipboard_manager, lb2);
+	xmi_msim_gui_clipboard_manager_register_widget(clipboard_manager, entry);
+	g_signal_connect(clipboard_manager, "update-clipboard-buttons", G_CALLBACK(update_clipboard_buttons), NULL);
+	g_signal_connect_swapped(cut, "clicked", G_CALLBACK(xmi_msim_gui_clipboard_manager_cut), clipboard_manager);
+	g_signal_connect_swapped(copy, "clicked", G_CALLBACK(xmi_msim_gui_clipboard_manager_copy), clipboard_manager);
+	g_signal_connect_swapped(paste, "clicked", G_CALLBACK(xmi_msim_gui_clipboard_manager_paste), clipboard_manager);
 
 	gtk_widget_show_all(window);
 
