5f75222159dea3e2866321389c30f0c6246d7103
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-04-12 08:10:06 +0100

    xmi_gobject.c: create GTypes for xmi_composition and xmi_excitation

---
+++ src/xmi_gobject.c
@@ -0,0 +1,68 @@
+/*
+Copyright (C) 2018 Tom Schoonjans and Laszlo Vincze
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config.h>
+#include "xmi_gobject.h"
+#include "xmi_data_structs.h"
+
+static gpointer xmi_msim_composition_copy(gpointer boxed) {
+	g_debug("Copying XmiMsimComposition");
+	struct xmi_composition *A = boxed;
+	struct xmi_composition *B = NULL;
+	xmi_copy_composition(A, &B);
+	return B;
+}
+
+static void xmi_msim_composition_free(gpointer boxed) {
+	g_debug("Freeing XmiMsimComposition");
+	xmi_free_composition((struct xmi_composition *) boxed);
+}
+
+static gpointer xmi_msim_excitation_copy(gpointer boxed) {
+	g_debug("Copying XmiMsimExcitation");
+	struct xmi_excitation *A = boxed;
+	struct xmi_excitation *B = NULL;
+	xmi_copy_excitation(A, &B);
+	return B;
+}
+
+static void xmi_msim_excitation_free(gpointer boxed) {
+	g_debug("Freeing XmiMsimExcitation");
+	xmi_free_excitation((struct xmi_excitation *) boxed);
+}
+
+// taken more or less from ebassi's graphene-gobject.c
+#define XMI_MSIM_DEFINE_BOXED_TYPE(TypeName, type_name) \
+  GType \
+    type_name ## _get_type (void) \
+  { \
+    static volatile gsize xmi_msim_define_id__volatile = 0; \
+    if (g_once_init_enter (&xmi_msim_define_id__volatile)) \
+      { \
+        GType xmi_msim_define_id = \
+          g_boxed_type_register_static (g_intern_static_string (#TypeName), \
+                                        (GBoxedCopyFunc) type_name ## _copy, \
+                                        (GBoxedFreeFunc) type_name ## _free); \
+        g_once_init_leave (&xmi_msim_define_id__volatile, xmi_msim_define_id); \
+      } \
+    return xmi_msim_define_id__volatile; \
+  }
+
+
+
+XMI_MSIM_DEFINE_BOXED_TYPE(XmiMsimComposition, xmi_msim_composition);
+XMI_MSIM_DEFINE_BOXED_TYPE(XmiMsimExcitation, xmi_msim_excitation);
