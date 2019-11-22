d1242a559440328328377dd4b8fa46de46f29ca6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-11-22 13:32:59 +0000

    xmimsim-gui.h: rewrite to include all xmimsim-gui headers

---
+++ bin/xmimsim-gui-pp.h
@@ -0,0 +1,27 @@
+/*
+Copyright (C) 2019 Tom Schoonjans and Laszlo Vincze
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef XMI_MSIM_GUI_PP_H
+#define XMI_MSIM_GUI_PP_H
+
+#include "xmimsim-gui.h"
+
+// here follows a list of headers that contain C++ code
+#include "xmimsim-gui-export-canvas-dialog.h"
+
+#endif
+
