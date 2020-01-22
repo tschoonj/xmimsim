c65bda4b0a54b9457abfddfa901e0c58d52e56d5
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2020-01-22 15:28:41 +0000

    configure.ac: bump version to 8.0

--- include/xmi_msim.h
+++ include/xmi_msim.h
@@ -26,8 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
-#define XMI_MSIM_VERSION_MAJOR 7
-#define XMI_MSIM_VERSION_MINOR 2
+#define XMI_MSIM_VERSION_MAJOR 8
+#define XMI_MSIM_VERSION_MINOR 0
 
 #include "xmi_config.h"
 #include "xmi_random.h"
