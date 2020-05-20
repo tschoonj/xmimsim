ecd17fb43bb7b3f8694ad74729afbf4dd9e2d57c
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-05-20 13:20:12 +0100

    Set LANG to en_US.UTF-8
    This was causing problems with Python 3.8 on macOS.

--- bin/xmimsim-conv.c
+++ bin/xmimsim-conv.c
@@ -71,7 +71,7 @@ int main(int argc, char **argv) {
 #if defined(G_OS_WIN32)
 	setlocale(LC_ALL,"English_United States");
 #else
-	g_setenv("LANG","en_US",TRUE);
+	g_setenv("LANG", "en_US.UTF-8",TRUE);
 #endif
 
 #ifdef G_OS_WIN32
